97834af0462316823e578ffec79e8b9babf753f4
EiriniChalatsiadou <canadian14@gmail.com>
2023-03-30 11:21:02 +0000

    add top/bottom user input, validator and user loop

--- run.py
+++ run.py
@@ -51,33 +51,37 @@ def get_player_stat_option():
     print('0) Quit\n')
     data_option = input('Please, enter your option (a number 0-7): ')
     return data_option
+    
 
-# def get_top_bottom_players_option():
-#     """
-#     This is the function with the players stats options
-#     """
-#     print('What do you want to know about? Please, choose an option 1-8')
-#     print('1) Points\n')
-#     print('2) Steals\n')
-#     print('3) Quit\n')
-#     data_option = input('Please, enter your option (a number 1-3): ')
-#     return data_option
+def get_top_bottom_players_option(stat):
+    """
+    This is the function with the choose, top or bottom players
+    """
+    print(f'Regarding {stat}, do you want to select players from top or bottom? Please, choose an option 0-2 ')
+    print('1) Top (best players)\n')
+    print('2) Bottom (least best players)\n')
+    print('0) Quit\n')
+    data_option = input('Please, enter your option (a number 0-2): ')
+    return data_option
 
-# def validate_top_bottom_option(top_bottom_option):
-#     """
-#     xf
-#     """
-#     try:
-#         player_int_option = int(option)
-#         if (player_int_option < 1 or player_int_option > 8):
-#             raise ValueError()
-#         if player_int_option == 8:
-#             print("Thank you for using our program! Bye!!")
-#             exit()
-#     except ValueError:
-#         return False
 
-#     return True    
+def validate_top_bottom_option(option_string):
+    """
+     Validate top_bottom_option to be an integer number between 0-2
+     Raises ValueError if not integer or if not in this range.
+     Also prints the error.
+    """
+    try:
+        top_bottom_option = int(option_string)
+        if (top_bottom_option < 0 or top_bottom_option > 2):
+            raise ValueError()
+        if top_bottom_option == 0:
+            print("Thank you for using our program! Bye!!")
+            exit()
+    except ValueError:
+        return False
+
+    return True    
 
 
 def validate_username(username):
@@ -247,6 +251,16 @@ stat_options = {1: "PTS", 2: "STL", 3: "BLK",
 #     else:
 #         clear_screen()
 #         print("Invalid data. Please input a correct option integer [1-8]\n")
+
+player_stat = 4
+
+while True:
+    top_bottom_option = get_top_bottom_players_option(stat_options[player_stat])
+    if validate_top_bottom_option(top_bottom_option):
+        break
+    else:
+        clear_screen()
+        print("Invalid data. Please input a correct option integer [0-2]\n")
 
 
 data = player_total_stats.get_all_values()
